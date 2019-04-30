f61185305fe4bb68e5c5e4d3214973c37576c9cd
Ivan Zgoniaiko <zgoniaiko@gmail.com>
2019-05-01 01:02:57 +0300

    Add action for post quotes

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
 use App\Entity\Quote;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class QuoteController extends AbstractFOSRestController
 {
@@ -32,5 +35,33 @@ class QuoteController extends AbstractFOSRestController
         }
 
         return $this->handleView($this->view($quote));
+    }
+
+    /**
+     * @Annotations\View()
+     *
+     * @param Request $request
+     */
+    public function postQuoteAction(Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $em = $this->getDoctrine()->getManager();
+        $author = $em->getRepository(Author::class)->findOneByName($data['author']);
+        if (!$author) {
+            $author = (new Author())
+                ->setName($data['author']);
+
+            $em->persist($author);
+        }
+
+        $quote = (new Quote())
+            ->setAuthor($author)
+            ->setQuote($data['quote']);
+
+        $em->persist($quote);
+        $em->flush();
+
+        return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
     }
 }
