781a7fb2b86630b56c524d655b1945ba6e1cf503
Ivan Zgoniaiko <zgoniaiko@gmail.com>
2019-05-01 13:53:39 +0300

    Add test for create quote

--- tests/Controller/QuoteControllerTest.php
+++ tests/Controller/QuoteControllerTest.php
@@ -16,4 +16,25 @@ class QuoteControllerTest extends WebTestCase
         $this->assertJson($response->getContent());
         $this->assertEquals('[]', $response->getContent());
     }
+
+    public function testPostShouldCreateQoute()
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/quotes',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            \json_encode([
+                'quote' => 'test quote',
+                'author' => 'someone',
+            ])
+        );
+
+        $this->assertSame(201, $client->getResponse()->getStatusCode());
+        $response = $client->getResponse();
+        $this->assertJson($response->getContent());
+        $this->assertEquals('{"status":"ok"}', $response->getContent());
+    }
 }
