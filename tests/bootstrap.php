6db34531c683c5c626d9c51776247a0ca5592bc6
Ivan Zgoniaiko <zgoniaiko@gmail.com>
2019-05-01 13:12:18 +0300

    Enable mysql DB for tests and add bootstrap for re-create schema

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_ENV['BOOTSTRAP_RESET_DATABASE']) && $_ENV['BOOTSTRAP_RESET_DATABASE'] == true) {
+    echo "Resetting test database...";
+    passthru(sprintf(
+        'php "%s/../bin/console" doctrine:schema:drop --env=test --force --no-interaction',
+        __DIR__
+    ));
+    passthru(sprintf(
+        'php "%s/../bin/console" doctrine:schema:update --env=test --force --no-interaction',
+        __DIR__
+    ));
+    echo " Done" . PHP_EOL . PHP_EOL;
+}
+
+require __DIR__.'/../vendor/autoload.php';
