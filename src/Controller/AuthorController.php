4d513e94e0df9e89a6f1c9c027fff8fdde5c9e4a
Ivan Zgoniaiko <zgoniaiko@gmail.com>
2019-05-03 11:40:28 +0300

    Add controller for authors and routes for it

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Author;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations;
+
+class AuthorController extends AbstractFOSRestController
+{
+    /**
+     * @Annotations\View(templateVar="quote")
+     *
+     * @param int $id
+     */
+    public function getAuthorAction($id)
+    {
+        $author = $this->getDoctrine()->getRepository(Author::class)->find($id);
+
+        if (!$author) {
+            throw $this->createNotFoundException('Author does not exists');
+        }
+
+        return $this->handleView($this->view($author));
+    }
+}
